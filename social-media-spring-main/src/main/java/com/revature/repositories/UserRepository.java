5c83a0d64c9c7d648708eba4267ac0696142adef
William Price <wbprice3@yahoo.com>
2022-11-28 17:37:04 -0600

    updated for pwchange and user bug on front end

--- social-media-spring-main/src/main/java/com/revature/repositories/UserRepository.java
+++ social-media-spring-main/src/main/java/com/revature/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.revature.repositories;
 import com.revature.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByEmailAndPassword(String email, String password);
+    Optional<User> findByEmail(String email);
 }
